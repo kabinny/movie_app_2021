9ed9843ead349a64212cde8755c182408d888912
kabinny <kabinny@gmail.com>
2021-11-11 22:32:10 +0900

    04-3 음식 앱 이리저리 만지고, 고쳐보기

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function Food({ name, picture }) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} width="100%" />
+      <img src={picture} alt={name} width="100%" />
     </div>
     
   );
@@ -12,22 +12,27 @@ function Food({ name, picture }) {
 
 const foodILike = [
   {
+    id: 1,
     name: 'Kimchi',
     image: 'https://images.unsplash.com/photo-1583224964978-2257b960c3d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
   },
   {
+    id: 2,
     name: 'Samgyeopsal',
     image: 'https://images.unsplash.com/photo-1548150914-c9f19106dbf6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80',
   },
   {
+    id: 3,
     name: 'Bibimbap',
     image: 'https://images.unsplash.com/photo-1600289031464-74d374b64991?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2275&q=80',
   },
   {
+    id: 4,
     name: 'Doncasu',
     image: 'https://images.unsplash.com/photo-1624517607896-bb5dfd8f5764?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2670&q=80',
   },
   {
+    id: 5,
     name: 'Kimbap',
     image: 'https://images.unsplash.com/photo-1608731001805-187e9c904388?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
   },
@@ -37,7 +42,7 @@ function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   )
